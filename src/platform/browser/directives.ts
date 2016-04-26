70c7d0423adbc98ff01884faa51f03cc47d482e5
Iliya Tryapitsin <iliya.tryapitsin@arcusys.fi>
2016-04-26 13:35:06 +0300

    Turn test on

--- src/platform/browser/directives.ts
+++ src/platform/browser/directives.ts
@@ -10,7 +10,7 @@ import {ROUTER_DIRECTIVES} from 'angular2/router';
 
 // application_directives: directives that are global through out the application
 export const APPLICATION_DIRECTIVES = [
-  ...ROUTER_DIRECTIVES,
+  ...ROUTER_DIRECTIVES
 ];
 
 export const DIRECTIVES = [
